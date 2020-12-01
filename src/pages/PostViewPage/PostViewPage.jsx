1571d30a0df417be475e61688dca360a0980a645
Martyna Matusek <martynamatusek@gmail.com>
2020-12-01 23:17:55 +0100

    [TASK] create post view page

---
+++ src/pages/PostViewPage/PostViewPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { PostDetailsPageContainer } from "../../containers";
+
+export const PostViewPage = () => {
+  return <PostDetailsPageContainer />;
+};
---
+++ src/pages/PostViewPage/index.js
@@ -0,0 +1,1 @@
+export { PostViewPage } from "./PostViewPage";
